05f53b1149b978a1b9b10f2f3e0c2a430485882e
Lidor Ashush <lidor12456@gmail.com>
2022-12-15 23:51:13 +0200

    disable env

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 // `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.xdyb6xu.mongodb.net/?retryWrites=true&w=majority`;
 
-const URL = `mongodb+srv:${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nekqmgn.mongodb.net/?retryWrites=true&w=majority`;
-
+const URL = `mongodb+srv://lidor_ashush:9w2IGr1PCRrlD1cw@cluster0.nekqmgn.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(URL, (err, mongoDbInstance) => {
   if (err) {
     throw Error("MongoDB connection error: " + err);
