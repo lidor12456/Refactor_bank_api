fe56e1f819455b3304e0b2ef339343771f8ec9c5
Lidor Ashush <lidor12456@gmail.com>
2022-12-16 11:42:29 +0200

    routes and funcs

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -15,3 +15,53 @@ export const addUser = async (req, res) => {
   const newUser = await addUserToMongo(body);
   res.status(201).send(newUser);
 };
+
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await bankUsers.findById(id);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
+
+export const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedData = await bankUsers.findByIdAndUpdate(
+      id,
+      { cash: req.body.cash, credit: req.body.credit },
+      { new: true }
+    );
+    res.status(201).send(updatedData);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
+
+export const transferCash = async (req, res) => {
+  const { firstId } = req.params;
+  const { secondId } = req.params;
+
+  const transferredCash = +req.body.cash;
+  try {
+    let giver = await bankUsers.findById(firstId);
+    let receiver = await bankUsers.findById(secondId);
+
+    giver = await bankUsers.findByIdAndUpdate(
+      firstId,
+      { cash: giver.cash - transferredCash },
+      { new: true }
+    );
+    receiver = await bankUsers.findByIdAndUpdate(
+      secondId,
+      { cash: receiver.cash + transferredCash },
+      { new: true }
+    );
+    res.status(201).send(receiver);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+};
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { getAllUsers, addUser } from "../controllers/users.controller.js";
+import {
+  getAllUsers,
+  addUser,
+  getUserById,
+  updateUserById,
+  transferCash,
+} from "../controllers/users.controller.js";
 import { userAuth } from "../middlewares/auth.middleware.js";
 export const indexRoute = Router();
 
-indexRoute.get("/index", getAllUsers);
+indexRoute.get("/allusers", getAllUsers);
+indexRoute.get("/:id", getUserById);
 indexRoute.post("/adduser", addUser);
+indexRoute.post("/:id", updateUserById);
+indexRoute.post("/transfercash/:firstId/:secondId", transferCash);
